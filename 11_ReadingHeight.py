05d6d8b97ba8b3fb6e518ec62ec0932128644e76
gitvrt <valluriraviteja@gmail.com>
2014-07-13 02:11:21 +0530

    Some more programs aftre 2nd push

---
+++ 11_ReadingHeight.py
@@ -0,0 +1,5 @@
+print "Please enter your height"
+height = raw_input()
+print "Your height in raw format is %r cms" % height
+print "Your height in String format is %s cms" % height
+#print "Your height in Float format is %f cms" % height
---
+++ example1.py
@@ -0,0 +1,3 @@
+print """Ravi
+teja
+valluri"""

1aaafe3509da2ebf51bf355459dbe288b807dbec
gitvrt <valluriraviteja@gmail.com>
2014-07-14 17:05:56 +0530

    Program Illustrating Unpacking and reading Command Line arguments

---
+++ 13_Unpacking_Arguments_reading.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script_name , arg1 , arg2 , arg3 = argv
+
+print "Your Script/Program name is %s \n First argument is %s \n Second argument is %s \n Third argument is %s " %(script_name,arg1,arg2,arg3)

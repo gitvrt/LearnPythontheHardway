0e82b55c9003afc4c24dc1c45a9c0a29dbceccbe
gitvrt <valluriraviteja@gmail.com>
2014-07-14 17:25:14 +0530

    Program that Illustrates formatted Prompting

---
+++ 14_Formatted_Promting.py
@@ -0,0 +1,17 @@
+prompt = '> '
+
+print "Hi! Please Answer my questions "
+
+print "What is your Name ? "
+name = raw_input(prompt)
+
+print "What is your Qualification ? "
+qual = raw_input(prompt)
+
+print "What is your Stream ? "
+stream = raw_input(prompt)
+
+print "What is your University name ? "
+univ = raw_input(prompt)
+
+print "Hi Mr/Mrs %s \n You pursued %s degree in %s from %s Universtiy " %(name,qual,stream,univ)

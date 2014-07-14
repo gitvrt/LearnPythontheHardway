440e038c389be2abcc9f578430af5f140251a21a
gitvrt <valluriraviteja@gmail.com>
2014-07-14 16:56:26 +0530

    Prompting user using Raw_input()

---
+++ 12_Prompting_using_rawInput().py
@@ -0,0 +1,5 @@
+Lap_manfacturer = raw_input("What is Your Laptop Manfacturer Name ? : ")
+Lap_model = raw_input("What is your laptop Model ? : ")
+Lap_harddisk = raw_input("What is your laptop hardisk capacity ? : ")
+
+print "You are using %r Laptop with model number : %r whose hardisk capacity is : %r GB " ,%(Lap_manfacturer,Lap_model,Lap_harddisk)

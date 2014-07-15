a16e26d0031e46d05fee6e353ebd6e73e915ee1b
gitvrt <valluriraviteja@gmail.com>
2014-07-15 20:29:47 +0530

    Program illustrating preformatting and escape sequences

---
+++ 24_Formatting_EscapeSequences.py
@@ -0,0 +1,17 @@
+print "Let's Practice Preformatting again"
+
+poem = """
+\t The lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation 
+\n\t\twhere there is none.
+"""
+
+print "--------------"
+print poem
+print "--------------"
+
+
+

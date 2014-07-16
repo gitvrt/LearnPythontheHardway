b28a9614e13ea5177db3cd82a3cc9505b28cc496
gitvrt <valluriraviteja@gmail.com>
2014-07-16 17:22:04 +0530

    Program on For-loop,lists and Sorting of lists

---
+++ 32_Loops_n_Lists.py
@@ -0,0 +1,37 @@
+names = ['rat','bat','cat','apple','dog','eagle']
+print "\nlength of the list is ",len(names)
+
+print "List elements are :"
+for name in names:
+	print "%s"%name
+
+
+
+print "\nSorted list after using sorted(names)\n"
+for name in sorted(names):
+	print "%s"%name
+
+print "\nReversing after using sorted(names,reverse=False)\n"
+for name in sorted(names,reverse=True):
+	print "%s"%name
+	
+newlist = []
+print "Now lets prepare another list"
+print "Please mention the list size : "
+lsize = int(raw_input('>'))
+
+print "\nPlease enter %d elements"%lsize
+for i in range(0,lsize):
+	newlist.append(int(raw_input()))
+
+print "New List elements are :"
+for ele in newlist:
+	print "%d"%ele
+	
+print "\nNew List after sorting are :"
+for ele in sorted(newlist):
+	print "%d"%ele
+
+print "\nNew List after Sorting in reverse Order are :"
+for ele in sorted(newlist,reverse=True):
+	print "%d"%ele

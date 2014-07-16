879832cf974cdeb81ac49cd4a411ad7b7a27788c
gitvrt <valluriraviteja@gmail.com>
2014-07-16 13:42:55 +0530

    Program on If-Else

---
+++ 30_If_else.py
@@ -0,0 +1,9 @@
+print "Welcome to TAXI HUB"
+print "Currently we have 2 cars, and can accommodate 8 people"
+
+people = int(raw_input("NO of people = "))
+
+if people>8:
+	print "Sorry we cant provide cars"
+else:
+	print "Yes we can provide you cars"

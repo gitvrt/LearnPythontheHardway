e41cd88fe978f0536da85e2a61ba494532a63443
gitvrt <valluriraviteja@gmail.com>
2014-07-17 16:32:25 +0530

    More operations on lists

---
+++ 38_More_on_Lists.py
@@ -0,0 +1,26 @@
+numbers = "One Two Three Four Five Six"
+
+print "Now we will split the above string using split()"
+
+stuff = numbers.split(' ')
+more_stuff = ["Day" , "Night" , "Song" , "Frisbee" , "Corn" , "Banana" , "Girl" , "Boy"]
+
+print "After splitting numbers : ",stuff," \nLength of the splitted list is",len(stuff)
+
+while len(stuff) !=10:
+	next_one = more_stuff.pop()
+	print "\n I am adding :",next_one
+	stuff.append(next_one)
+	print "There are %d items in the stuff now"%len(stuff)
+	
+print "And finally stuff is ",stuff
+
+print "\n some more operations on stuff"
+
+print "stuff[1] is ",stuff[1]
+print "stuff[-1] is ",stuff[-1]
+print "stuff[-3] is ",stuff[-3]
+print "stuff.pop() is ",stuff.pop()
+print "' '.join(stuff) is ",' '.join(stuff)
+print "'#'.join(stuff[3:5]) is ",'#'.join(stuff[3:5])
+print "Final stuff ",stuff

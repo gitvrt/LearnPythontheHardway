9d8230c0949e2bd241c98480fa350a75a9b18bb1
gitvrt <valluriraviteja@gmail.com>
2014-07-17 17:20:39 +0530

    Program on Dictionaries

---
+++ 39_Dictionaries.py
@@ -0,0 +1,13 @@
+print "-"*5,"\nUsing Dictionaries {'key' : 'value'} n","-"*5
+
+std_code = {'hyd':'040' , 'blr':'080' , "chn":"044"}
+
+print "\n STD-CODE of HYDERABAD is ",std_code['hyd']
+
+print "\n STD-CODE of BANGLORE is ",std_code['blr']
+
+print "\n STD-CODE of CHENNAI is ",std_code['chn']
+
+std_code['kkr'] = "033"
+
+print std_code
